646191c716f580d9f442d8eaa62dcbc24e3dad15
echen94 <chenyiying@ucla.edu>
2018-04-14 10:33:15 -0700

    did 1-2a to d, working on e

---
+++ Project1/1-2.R
@@ -0,0 +1,26 @@
+library('igraph', lib="/Users/evelyn/Library/R")
+library('Matrix', lib="/Users/evelyn/Library/R")
+library('pracma', lib="/Users/evelyn/Library/R")
+
+#1-2a
+g1 <- barabasi.game(1000, m=1, directed=F)
+plot(g1,vertex.size=8, vertex.label=NA)
+is.connected(g1)
+
+#1-2b
+fc <- cluster_fast_greedy(g1)
+modularity(fc)
+
+#1-2c
+g2 <- barabasi.game(10000, m=1, directed=F)
+plot(g2,vertex.size=8, vertex.label=NA)
+fc2 <- cluster_fast_greedy(g2)
+modularity(fc2)
+
+#1-2d
+plot(degree.distribution(g1),main="Degree distribution of the network with 1000 nodes",xlab="Degree",ylab="Frequency",log='xy')
+plot(degree.distribution(g2),main="Degree distribution of the network with 10000 nodes",xlab="Degree",ylab="Frequency",log='xy')
+
+#1-2e
+vcount(g1)
+neighbors(g1, 1)
