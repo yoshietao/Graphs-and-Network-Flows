066bce9cba010c39284f3d046b04e7c2c4183d78
yoshietao <yoshietao@g.ucla.edu>
2018-04-14 18:25:06 -0700

    1-3 with save plot

--- Project1/1-3.R
+++ Project1/1-3.R
@@ -6,7 +6,13 @@ library('pracma', lib="/Users/evelyn/Library/R")
 g1 <- sample_pa_age(1000, pa.exp=1, aging.exp=-1, aging.bin=1000, directed=F)
 plot(g1,vertex.size=4, vertex.label=NA)
 plot(degree.distribution(g1),main="Degree distribution of 1-3",xlab="Degree",ylab="Frequency")
+#dev.copy(jpeg,'/Volumes/Transcend/UCLA/232E/project/Project1/1-3a_degree_distribution.jpg')
+#dev.off()
+
 plot(degree.distribution(g1),main="Degree distribution-loglog of 1-3",xlab="Degree",ylab="Frequency",log='xy')
+print(degree.distribution(g1))
+#dev.copy(jpeg,'/Volumes/Transcend/UCLA/232E/project/Project1/1-3a_degree_distribution_loglog.jpg')
+#dev.off()
 
 #1-3b
 fc1 <- cluster_fast_greedy(g1)
