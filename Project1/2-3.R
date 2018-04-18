ed85f390878cc524539778f2eeb65b283c28f739
yoshietao <yoshietao@g.ucla.edu>
2018-04-18 00:43:38 -0700

    4/17

--- Project1/2-3.R
+++ Project1/2-3.R
@@ -8,18 +8,18 @@ library('Matrix', lib="/Users/evelyn/Library/R")
 library('pracma', lib="/Users/evelyn/Library/R")
 library('data.table', lib="/Users/evelyn/Library/R")
 
-create_transition_matrix = function (g,alpha=0){
-  
-  adj = as_adjacency_matrix(g)
-  
-  adj_ = adj
-  adj_[adj==1] = 0
-  adj_[adj==0] = 1
-  
-  t1 <- get_t_matrix(g, adj)
-  t2 <- get_t_matrix(g, adj_)
-  return(alpha*t2+(1-alpha)*t1)
-}
+# create_transition_matrix = function (g,alpha=0){
+#   
+#   adj = as_adjacency_matrix(g)
+#   
+#   adj_ = adj
+#   adj_[adj==1] = 0
+#   adj_[adj==0] = 1
+#   
+#   t1 <- get_t_matrix(g, adj)
+#   t2 <- get_t_matrix(g, adj_)
+#   return(alpha*t2+(1-alpha)*t1)
+# }
 
 get_t_matrix <- function(g,adj){
   adj[diag(rowSums(adj) == 0)] = 1  # handle if the user is using the function for networks with isolated nodes by creating self-edges
@@ -28,10 +28,8 @@ get_t_matrix <- function(g,adj){
   return(transition_matrix)
 }
 
-random_walk = function (g, num_steps, start_node, transition_matrix = NULL, alpha=0){
-  if(is.null(transition_matrix))
-    transition_matrix = create_transition_matrix(g,alpha)
-  
+random_walk = function (g, num_steps, start_node, t1, t2, alpha=0){
+    
   his = c()
   v = start_node
   for(i in 1:num_steps){
@@ -40,39 +38,58 @@ random_walk = function (g, num_steps, start_node, transition_matrix = NULL, alph
     if(v==1 && alpha==0){
       break
     }
-    PMF = transition_matrix[v, ]
-    v = sample(1:vcount(g), 1, prob = PMF) 
+    x = sample(0:1,1)
+    if(x<=0.15){
+      PMF = t2[v, ]
+      v = sample(1:vcount(g), 1, prob = PMF) 
+    }
+    else{
+      PMF = t1[v, ]
+      v = sample(1:vcount(g), 1, prob = PMF) 
+    }
   }
-  
   return(his)
 }
 
-g1 = sample_pa(1000,m=4)
-trans_mat <- create_transition_matrix(g1)
-his_arr = matrix(0,1,1000)
-
 #2-3a
-for(i in c(1:1000,1:1000)){
-  dists = c();
+g = sample_pa(1000,m=4)
+trans_mat <- create_transition_matrix(g1)
+p = matrix(0,1,1000)
+
+adj = as_adjacency_matrix(g)
+adj_ = adj
+adj_[adj==1] = 0
+adj_[adj==0] = 1
+t1 <- get_t_matrix(g, adj)
+t2 <- get_t_matrix(g, adj_)
+
+for(i in c(1:1000)){
   print(i)
-  his= random_walk(g1, 10, i, trans_mat)
-  his_arr[his] = his_arr[his]+1
+  his= random_walk(g, 10, i, t1, t2)
+  for(i in his){p[i] = p[i]+1}
 }
-his_arr = his_arr/2000
-print(his_arr)
-plot(1:1000,his_arr,type='l',main="Probability that the walker visits each node",xlab="Node added at time t",ylab="Probability")
+p = p/10000
+print(p)
+plot(1:1000,p,type='l',main="Probability that the walker visits each node",xlab="Node added at time t",ylab="Probability")
 
 #2-3b
-trans_mat <- create_transition_matrix(g1,0.15)
-his_arr = matrix(0,1,1000)
+adj = as_adjacency_matrix(g)
+adj_ = adj
+adj_[adj==1] = 0
+adj_[adj==0] = 1
+t1 <- get_t_matrix(g, adj)
+t2 <- get_t_matrix(g, adj_)
+#trans_mat <- create_transition_matrix(g1,0.15)
+p = matrix(0,1,1000)
 
-for(i in c(1:1000,1:1000)){
-  dists = c();
+for(i in c(1:100)){
   print(i)
-  his= random_walk(g1, 20, i, trans_mat, 0.15)
-  his_arr[his] = his_arr[his]+1
+  his= random_walk(g, 1000, i, t1, t2, 0.15)
+  for(i in his){p[i] = p[i]+1}
 }
-his_arr = his_arr/2000
-print(his_arr)
-plot(1:1000,his_arr,type='l',main="Probability that the walker visits each node",xlab="Node added at time t",ylab="Probability")
+
+p = p/100000
+print(p)
+plot(1:1000,p,type='l',main="Probability that the walker visits each node",xlab="Node added at time t",ylab="Probability")
+
 
