57a473b9d02c326da5121976239f65ed4fd6bce7
echen94 <chenyiying@ucla.edu>
2018-04-29 17:50:53 -0700

    finished problem 19 coding: create in and out degree for three personalized networks

--- Project2/2-1.R
+++ Project2/2-1.R
@@ -3,11 +3,8 @@ library('Matrix')
 library('pracma')
 library('data.table')
 
+# Problem 18 
 # create directed personal networks for users who have more than 2 circles
-#file = 'gplus/101541879642294398860.circles'
-#d <- read.table(file, sep = " ")
-#dim(d)[1] # number of circles in the file 
-
 path = "gplus/"
 out.file<-""
 file.names <- dir(path, pattern =".circles")
@@ -29,4 +26,30 @@ for (i in 1:length(file.names)){
 }
 # return count 
 count
+
+# Problem 19
+# Create Directed Graphs from the nodeId.edges files 
+# file = "test.txt"
+node_list <- c("109327480479767108490", 
+               "115625564993990145546",
+               "101373961279443806744")
+extension <- ".edges"
+for (node_num in node_list){
+  graph_list = c() 
+  d <- read.table(paste0(path,node_num,extension), sep = " ")
+  g <- graph_from_data_frame(d,directed=TRUE)
+  plot(g, vertex.size=4, vertex.label=NA)
+  
+  # plot degree distribution for out degree 
+  degrees <- degree(g, mode="out")
+  degreeNum <- as.numeric(names(degrees))
+  degreeFreq <- unname(degrees)/sum(degrees)
+  plot(degreeNum, degreeFreq,main=paste("Degree Distribution for out-degree for node", node_num),xlab="Degree",ylab="Frequency")
+  
+  # plot degree distribution for in degree 
+  degrees <- degree(g, mode="in")
+  degreeNum <- as.numeric(names(degrees))
+  degreeFreq <- unname(degrees)/sum(degrees)
+  plot(degreeNum, degreeFreq,main=paste("Degree Distribution for in-degree for node", node_num),xlab="Degree",ylab="Frequency")
+}
 
