06b170f6bef8d1af1dacbadb6a53ac0ce8bacad9
echen94 <chenyiying@ucla.edu>
2018-05-27 22:46:54 -0700

    part 1 Q1~Q5

--- Project4/project4_1.R
+++ Project4/project4_1.R
@@ -2,12 +2,7 @@ library(igraph)
 library(data.table)
 
 setwd("/Users/evelyn/Documents/master_first_year/third_quarter/ECE232/Random-Graphs-and-Random-Walks/Project4/")
-# fileName="project4_data/actor_movies.txt"
-# files = c("test2.txt", "test3.txt")
 files = c("project4_data/actor_movies.txt", "project4_data/actress_movies.txt")
-#con=file(fileName,open="r")
-#lines = readLines(con)
-
 StartTime <- Sys.time()
 
 # DT = data.table(A=NA, M=NA)
@@ -15,7 +10,7 @@ data_list <- list()
 i <- 1
 for (filename in files){
   print (filename)
-  con=file(filename,open="r")
+  con=file(filename,open="r",encoding='ISO-8859-1')
   lines = readLines(con)
   for (l in lines){
     tokens = unlist(strsplit (l, '\t\t'))
@@ -24,9 +19,6 @@ for (filename in files){
     {
       mov = trimws(tokens [2:length(tokens)])
       mov = gsub ("(.+)\\s\\s.*", "\\1", mov)
-      #DT_tmp = data.table(A=act, M=mov)
-      #DT <- rbind(DT, DT_tmp)
-      # datalist[[i]] <- data.table(name=, movie=)
       data_list[[i]] <- data.table(A=act, M=mov)
       i <- i+1 
     }
@@ -63,3 +55,35 @@ saveRDS(pair_DT, file = "pair_DT_data.rds")
 g <- graph_from_edgelist(as.matrix(pair_DT[,.(id.x,id.y)]))
 plot(degree.distribution(g, mode="in"), main=paste("Degree Distribution for in-degree for graph"), xlab="degree", ylab="frequency", cex = 0.7, cex.axis=0.8, cex.main=0.7)
 
+getPairing <- function(nameToLookup, pairDT, idDT) {
+  setorder(pairDT, id.x, -wt)
+  id <- idDT[name == nameToLookup]$id
+  outputId <- pairDT[id.x == id]$id.y[1]
+  outputName <- idDT[id == outputId]$name
+  outputWt <- pairDT[id.x == id]$wt[1]
+  cat("Input: ", nameToLookup, ", Output: ", outputName, ", Weight: ", outputWt, "\n")
+}
+
+actorVec <- c("Cruise, Tom", "Watson, Emma (II)", "Clooney, George", "Hanks, Tom", "Johnson, Dwayne (I)",
+              "Depp, Johnny", "Smith, Will (I)", "Streep, Meryl", "DiCaprio, Leonardo", "Pitt, Brad")
+
+invisible(sapply(actorVec, getPairing, pairDT, idDT))
+
+print("get pair time:")
+Sys.time()-init2
+
+init3 <-Sys.time()
+
+pageRank <- page_rank(g)
+saveRDS(pageRank, paste0(inputDir, "pageRank.rds"))
+
+ranking <- rank(-(pageRank$vector))
+top10Id <- match(c(1:10), ranking)
+top10DT <- idDT[top10Id]
+top10DT <- merge(top10DT, dataDT[, .(numMovies = .N), by = .(id)], by = "id", sort = F)
+top10DT <- merge(top10DT, pairDT[, .(inDegree = .N), by = id.y], by.x = "id", by.y = "id.y", sort = F)
+
+print("page rank time: ")
+Sys.time()-init3
+
+top10DT
