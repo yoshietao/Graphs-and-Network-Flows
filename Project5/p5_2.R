47e8b974aa6153509e66bb893a657aa2598b451a
echen94 <chenyiying@ucla.edu>
2018-06-09 00:24:10 -0700

    fixed Q8 with faster method

--- Project5/p5_2.R
+++ Project5/p5_2.R
@@ -3,9 +3,9 @@ library(data.table)
 library(rjson)
 
 DO_6 = TRUE
-DO_7 = TRUE
-DO_8 = FALSE
-DO_9 = TRUE
+DO_7 = FALSE
+DO_8 = TRUE
+DO_9 = FALSE
 
 #setwd("/Users/evelyn/Documents/master_first_year/third_quarter/ECE232/Random-Graphs-and-Random-Walks/Project5/")
 filename = 'san_francisco-censustracts-2017-4-All-MonthlyAggregate.csv'
@@ -83,41 +83,28 @@ if (DO_7 == TRUE){
 
 #Q8
 if (DO_8 == TRUE){
-  triangles_index <- triangles(g_sub) # this will output a list of triangles, first three vertices represent first triangle...etc 
-  # pick 1000 from number of triangles
-  rand_1000_triangles_index <- sample(1:length(triangles_index)/3, 1000, replace=F)
-  length(rand_1000_triangles_index)
-  rand_1000_triangles <- c()
   tri_ineq_true <- 0
   tri_ineq_false <- 0
-  euc.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
-  for (t in rand_1000_triangles_index){
-    pt1 <- triangles_index[(t-1)*3+1]
-    pt2 <- triangles_index[(t-1)*3+2]
-    pt3 <- triangles_index[(t-1)*3+3]
-    v_list <- get.edge.ids(g_sub, c(pt1, pt2, pt1, pt3, pt2, pt3))
+  count <- 0
+  while (count < 1000){
+    v <- sample(1:vcount(g_sub), 1, replace=FALSE)
+    neigh_list <- neighbors(g_sub, v)
+    random_two <- sample (neigh_list, size=2, replace=FALSE)
+    while ( (are.connected(g_sub, random_two[1], random_two[2]) == FALSE)){
+      random_two <- sample (neigh_list, size=2, replace=FALSE)
+    }
+    are.connected(g_sub, random_two[1], random_two[2])
+    v_list <- get.edge.ids(g_sub, c(v, random_two[1], v, random_two[2], random_two[1], random_two[2]))
     weights <- edge_attr(g_sub, "mtt", index = v_list)
-    # v_list = c(pt1,pt2,pt3)
-    # x_coord <- vertex_attr(g_sub, "x", v_list)
-    # y_coord <- vertex_attr(g_sub, "y", v_list)
-    # dist1_2 <- euc.dist(c(x_coord[1],y_coord[1]),c(x_coord[2],y_coord[2]))
-    # dist2_3 <- euc.dist(c(x_coord[2],y_coord[2]),c(x_coord[3],y_coord[3]))
-    # dist1_3 <- euc.dist(c(x_coord[1],y_coord[1]),c(x_coord[3],y_coord[3]))
-    # if (dist1_2 + dist2_3 > dist1_3 && dist1_2 + dist1_3 > dist2_3 && dist2_3 + dist1_3 > dist1_2){
-    #  tri_ineq_true <- tri_ineq_true + 1
-    #}
-    #else{
-    #  tri_ineq_false <- tri_ineq_false + 1
-    #}
     if (weights[1]+weights[2]>weights[3] && weights[1]+weights[3]>weights[2] && weights[2]+weights[3]>weights[1])
       tri_ineq_true <- tri_ineq_true + 1
-    else
+    else 
       tri_ineq_false <- tri_ineq_false + 1
+    count <- count +1
   }
   tri_ineq_true
   tri_ineq_false
   
-  # return tri_ineq_true / (tri_ineq_true + tri_ineq_false)
 }
 
 #Q9---->map missing path to time weighted shortest path(distance)
